42c1445d2ee34b366056cd0a8f878e093088a934
Cristi Sava <cristirock69@gmail.com>
2025-12-08 18:44:16 +0000

    it still is

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,8 @@
 
 export function Logo() {
   return (
-    <div className="flex items-center" aria-label="ATFR Logo">
-      <img src="/logo_navbar_tight.png" alt="ATFR Logo" width="56" height="56" />
+    <div className="flex items-center justify-center h-12 w-12" aria-label="ATFR Logo">
+      <img src="/logo_navbar_tight.png" alt="ATFR Logo" className="object-contain h-full w-full" />
     </div>
   );
 }
