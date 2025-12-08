0dfd6464ed1205650e03c5377e31a28f80b8e59b
Cristi Sava <cristirock69@gmail.com>
2025-12-08 18:43:51 +0000

    the logo is now cut at the top and bottom, fix it

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@
 export function Logo() {
   return (
     <div className="flex items-center" aria-label="ATFR Logo">
-      <img src="/logo_navbar_tight.png" alt="ATFR Logo" width="64" height="64" />
+      <img src="/logo_navbar_tight.png" alt="ATFR Logo" width="56" height="56" />
     </div>
   );
 }
